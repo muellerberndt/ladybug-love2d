20f50b2818cc8bf83e12c082233c566cfc34887a
Bernhard Mueller <bernhardmueller@multipoarneuron.lan>
2023-01-19 12:49:01 +0700

    Move RNG level data to LevelStartState

--- states/LevelStartState.lua
+++ states/LevelStartState.lua
@@ -8,18 +8,32 @@ end
 
 function LevelStartState:draw()
     love.graphics.setFont(largeFont)
-    love.graphics.print("Level " .. self.level .. " start!", 40, 100)
+    love.graphics.print("Level " .. self.level .. " start!", 42, 100)
+    love.graphics.print(self.extraLetter.. " - ".. self.specialLetter .. " - " .. self.commonLetter, 59, 120)
 end
 
 function LevelStartState:enter(params)
     self.level = params.level or 1
+    self.lives = params.lives or 3
+    self.score = params.score or 0
 
-    Timer.after(1, function()
+    -- RNG stuff
+
+    self.extraLetter = EXTRA_LETTERS[love.math.random(#EXTRA_LETTERS)]
+    self.specialLetter = SPECIAL_LETTERS[love.math.random(#SPECIAL_LETTERS)]
+    self.commonLetter = COMMON_LETTERS[love.math.random(#COMMON_LETTERS)]
+    self.nSkulls = love.math.random(3)
+
+    Timer.after(2, function()
         gStateMachine:change('play',
             {
                 level = self.level,
-                score = 0,
-                lives = 3
+                score = self.score,
+                lives = self.lives ,
+                extraLetter = self.extraLetter,
+                specialLetter = self.specialLetter,
+                commonLetter = self.commonLetter,
+                nSkulls = self.nSkulls
             })
     end)
 end
--- states/PlayState.lua
+++ states/PlayState.lua
@@ -8,130 +8,6 @@ function PlayState:reset()
     self.entityManager:reset()
 end
 
-function PlayState:setupLevel(level)
-
-    local data = LevelData[level]
-
-    self.entityManager = EntityManager()
-    self.entityManager:addEntity(Player(), EntityTypes.PLAYER)
-
-    local available_tiles = {}
-
-    for y, row in pairs(tilemap) do
-        for x, col in pairs(row) do
-            if col == 0 and x % 2 == 0 and y % 2 == 0
-            and not (y > 17 and x == 12)
-            and not (y == 12 and x == 12)
-            then
-                table.insert(available_tiles, {y, x})
-            elseif col == 1 then
-                local wall = Wall(5 + (x - 1) * 8, 14 + y * 8)
-                self.entityManager:addEntity(wall, EntityTypes.WALL)
-            end
-        end
-    end
-
-    local extra_letter = EXTRA_LETTERS[love.math.random(#EXTRA_LETTERS)]
-    local special_letter = SPECIAL_LETTERS[love.math.random(#SPECIAL_LETTERS)]
-    local common_letter = COMMON_LETTERS[love.math.random(#COMMON_LETTERS)]
-
-    local idx = math.random(#available_tiles)
-    local row = available_tiles[idx][1]
-    local col = available_tiles[idx][2]
-    local x, y = getPositionForTile(row, col)
-
-    self.entityManager:addEntity(Letter(x - 1, y - 1, extra_letter), EntityTypes.LETTER)
-    table.remove(available_tiles, idx)
-
-    idx = math.random(#available_tiles)
-    row = available_tiles[idx][1]
-    col = available_tiles[idx][2]
-    x, y = getPositionForTile(row, col)
-
-    self.entityManager:addEntity(Letter(x - 1, y - 1, special_letter), EntityTypes.LETTER)
-    table.remove(available_tiles, idx)
-
-    idx = math.random(#available_tiles)
-    row = available_tiles[idx][1]
-    col = available_tiles[idx][2]
-    x, y = getPositionForTile(row, col)
-
-    self.entityManager:addEntity(Letter(x - 1, y - 1, common_letter), EntityTypes.LETTER)
-    table.remove(available_tiles, idx)
-
-    for i=1, 3, 1 do
-        idx = math.random(#available_tiles)
-        row = available_tiles[idx][1]
-        col = available_tiles[idx][2]
-        x, y = getPositionForTile(row, col)
-
-        self.entityManager:addEntity(Letter(x - 1, y - 1, "heart"), EntityTypes.LETTER)
-        table.remove(available_tiles, idx)
-    end
-
-    local numSkulls = math.random(3)
-
-    for i=1, numSkulls, 1 do
-        idx = math.random(#available_tiles)
-        row = available_tiles[idx][1]
-        col = available_tiles[idx][2]
-        x, y = getPositionForTile(row, col)
-
-        self.entityManager:addEntity(Skull(x - 1, y - 1, common_letter), EntityTypes.GAMEOBJECT)
-        table.remove(available_tiles, idx)
-    end
-
-    for i, tile in pairs(available_tiles) do
-        local row = available_tiles[i][1]
-        local col = available_tiles[i][2]
-
-        local x, y = getPositionForTile(row, col)
-
-        local flower = Flower(
-            x + 2,
-            y + 2
-        )
-        self.entityManager:addEntity(flower, EntityTypes.FLOWER)
-    end
-
-    for _, turnstile in pairs(turnstiles) do
-        self.entityManager:addEntity(Turnstile(turnstile.row, turnstile.col, turnstile.orientation), EntityTypes.TURNSTILE)
-    end
-
-    self.trappedEnemies = {}
-
-    for _, e in pairs(data.enemies) do
-
-        local enemy = Enemy(e, data.enemySpeed, self.entityManager)
-
-        self.entityManager:addEntity(
-            enemy,
-            EntityTypes.ENEMY
-        )
-
-        table.insert(self.trappedEnemies, enemy)
-    end
-
-    self.tick = 0
-
-    Timer.every(data.tick, function()
-        self.tick = self.tick + 1
-
-        if (self.tick == 82 or self.tick == 174) and #self.trappedEnemies > 0 then
-            sounds['enemylaunch']:play()
-        end
-        if (self.tick == 92 or self.tick == 184) and #self.trappedEnemies > 0 then
-            local idx = #self.trappedEnemies
-            self.trappedEnemies[idx].state = "roaming"
-            table.remove(self.trappedEnemies, idx)
-        end
-        if self.tick == 184 then
-            self.tick = 0
-        end
-    end)
-
-end
-
 function PlayState:update(dt)
     self.entityManager:update(dt)
 
@@ -142,7 +18,9 @@ function PlayState:update(dt)
         gStateMachine:change(
             "levelstart",
             {
-                level = self.level + 1
+                level = self.level + 1,
+                score = self.score,
+                lives = self.lives ,
             }
     )
     end
@@ -184,11 +62,129 @@ end
     Called when this state is transitioned to from another state.
 ]]
 function PlayState:enter(params)
-    self.level = params.level or 1
-    self.score = params.score or 0
-    self.lives = params.lives or 3
+    self.level = params.level
+    self.score = params.score
+    self.lives = params.lives
+    local extraLetter = params.extraLetter
+    local specialLetter = params.specialLetter
+    local commonLetter = params.commonLetter
+    local nSkulls = params.nSkulls
 
-    self:setupLevel(self.level)
+    -- Set up the level
+
+    local data = LevelData[self.level]
+
+    self.entityManager = EntityManager()
+    self.entityManager:addEntity(Player(), EntityTypes.PLAYER)
+
+    local available_tiles = {}
+
+    for y, row in pairs(tilemap) do
+        for x, col in pairs(row) do
+            if col == 0 and x % 2 == 0 and y % 2 == 0
+            and not (y > 17 and x == 12)
+            and not (y == 12 and x == 12)
+            then
+                table.insert(available_tiles, {y, x})
+            elseif col == 1 then
+                local wall = Wall(5 + (x - 1) * 8, 14 + y * 8)
+                self.entityManager:addEntity(wall, EntityTypes.WALL)
+            end
+        end
+    end
+
+    local idx = math.random(#available_tiles)
+    local row = available_tiles[idx][1]
+    local col = available_tiles[idx][2]
+    local x, y = getPositionForTile(row, col)
+
+    self.entityManager:addEntity(Letter(x - 1, y - 1, extraLetter), EntityTypes.LETTER)
+    table.remove(available_tiles, idx)
+
+    idx = math.random(#available_tiles)
+    row = available_tiles[idx][1]
+    col = available_tiles[idx][2]
+    x, y = getPositionForTile(row, col)
+
+    self.entityManager:addEntity(Letter(x - 1, y - 1, specialLetter), EntityTypes.LETTER)
+    table.remove(available_tiles, idx)
+
+    idx = math.random(#available_tiles)
+    row = available_tiles[idx][1]
+    col = available_tiles[idx][2]
+    x, y = getPositionForTile(row, col)
+
+    self.entityManager:addEntity(Letter(x - 1, y - 1, commonLetter), EntityTypes.LETTER)
+    table.remove(available_tiles, idx)
+
+    for i=1, 3, 1 do
+        idx = math.random(#available_tiles)
+        row = available_tiles[idx][1]
+        col = available_tiles[idx][2]
+        x, y = getPositionForTile(row, col)
+
+        self.entityManager:addEntity(Letter(x - 1, y - 1, "heart"), EntityTypes.LETTER)
+        table.remove(available_tiles, idx)
+    end
+
+    for i=1, nSkulls, 1 do
+        idx = math.random(#available_tiles)
+        row = available_tiles[idx][1]
+        col = available_tiles[idx][2]
+        x, y = getPositionForTile(row, col)
+
+        self.entityManager:addEntity(Skull(x - 1, y - 1), EntityTypes.GAMEOBJECT)
+        table.remove(available_tiles, idx)
+    end
+
+    -- for i, tile in pairs(available_tiles) do
+    --     local row = available_tiles[i][1]
+    --     local col = available_tiles[i][2]
+
+    --     local x, y = getPositionForTile(row, col)
+
+    --     local flower = Flower(
+    --         x + 2,
+    --         y + 2
+    --     )
+    --     self.entityManager:addEntity(flower, EntityTypes.FLOWER)
+    -- end
+
+    for _, turnstile in pairs(turnstiles) do
+        self.entityManager:addEntity(Turnstile(turnstile.row, turnstile.col, turnstile.orientation), EntityTypes.TURNSTILE)
+    end
+
+    self.trappedEnemies = {}
+
+    for _, e in pairs(data.enemies) do
+
+        local enemy = Enemy(e, data.enemySpeed, self.entityManager)
+
+        self.entityManager:addEntity(
+            enemy,
+            EntityTypes.ENEMY
+        )
+
+        table.insert(self.trappedEnemies, enemy)
+    end
+
+    self.tick = 0
+
+    Timer.every(data.tick, function()
+        self.tick = self.tick + 1
+
+        if (self.tick == 82 or self.tick == 174) and #self.trappedEnemies > 0 then
+            sounds['enemylaunch']:play()
+        end
+        if (self.tick == 92 or self.tick == 184) and #self.trappedEnemies > 0 then
+            local idx = #self.trappedEnemies
+            self.trappedEnemies[idx].state = "roaming"
+            table.remove(self.trappedEnemies, idx)
+        end
+        if self.tick == 184 then
+            self.tick = 0
+        end
+    end)
 
     self.letterAwardedHandler = Event.on('letterAwarded', function(letter)
 
