3f8475797af97a6b71b41c29a884a8c2aaa7f3dd
Bernhard Mueller <bernhardmueller@Multipolar-Neuron.local>
2023-01-20 15:39:31 +0700

    Add remaining enemy sprites

--- data/defs.lua
+++ data/defs.lua
@@ -13,20 +13,25 @@ EnemyTypes = {
     },
     SCARAB = {
         spritesheet = 'assets/graphics/scarab.png'
+    },
+    FLY = {
+        spritesheet = 'assets/graphics/fly.png'
+    },
+    PALMERS = {
+        spritesheet = 'assets/graphics/palmers.png'
+    },
+    WORM = {
+        spritesheet = 'assets/graphics/worm.png'
     }
 }
 
 LevelData = {
     {
         enemies = {
-            EnemyTypes.IDONTKNOW,
-            EnemyTypes.IDONTKNOW,
-            EnemyTypes.IDONTKNOW,
-            EnemyTypes.IDONTKNOW,
-            -- EnemyTypes.BEETLE,
-            -- EnemyTypes.BEETLE,
-            -- EnemyTypes.BEETLE,
-            -- EnemyTypes.BEETLE,
+            EnemyTypes.BEETLE,
+            EnemyTypes.BEETLE,
+            EnemyTypes.BEETLE,
+            EnemyTypes.BEETLE,
         },
         fruitName = "Cucumber",
         fruitFile = "assets/graphcs/cucumber.png",
@@ -45,45 +50,84 @@ LevelData = {
         fruitFile = "assets/graphcs/cucumber.png",
         fruitScore = 1000,
         tick = 0.3,
+        enemySpeed = 60
+    },
+    {
+        enemies = {
+            EnemyTypes.MANTIS,
+            EnemyTypes.MANTIS,
+            EnemyTypes.MANTIS,
+            EnemyTypes.MANTIS,
+        },
+        fruitName = "Cucumber",
+        fruitFile = "assets/graphcs/cucumber.png",
+        fruitScore = 1000,
+        tick = 0.09,
         enemySpeed = 70
     },
     {
         enemies = {
-            EnemyTypes.MANTIS,
-            EnemyTypes.MANTIS,
-            EnemyTypes.MANTIS,
-            EnemyTypes.MANTIS,
+            EnemyTypes.IDONTKNOW,
+            EnemyTypes.IDONTKNOW,
+            EnemyTypes.IDONTKNOW,
+            EnemyTypes.IDONTKNOW,
         },
         fruitName = "Cucumber",
         fruitFile = "assets/graphcs/cucumber.png",
         fruitScore = 1000,
-        tick = 0.2,
+        tick = 0.08,
+        enemySpeed = 80
+    },
+    {
+        enemies = {
+            EnemyTypes.SCARAB,
+            EnemyTypes.SCARAB,
+            EnemyTypes.SCARAB,
+            EnemyTypes.SCARAB,
+        },
+        fruitName = "Cucumber",
+        fruitFile = "assets/graphcs/cucumber.png",
+        fruitScore = 1000,
+        tick = 0.07,
+        enemySpeed = 90
+    },
+    {
+        enemies = {
+            EnemyTypes.FLY,
+            EnemyTypes.FLY,
+            EnemyTypes.FLY,
+            EnemyTypes.FLY,
+        },
+        fruitName = "Cucumber",
+        fruitFile = "assets/graphcs/cucumber.png",
+        fruitScore = 1000,
+        tick = 0.06,
         enemySpeed = 100
     },
     {
         enemies = {
-            EnemyTypes.IDONTKNOW,
-            EnemyTypes.IDONTKNOW,
-            EnemyTypes.IDONTKNOW,
-            EnemyTypes.IDONTKNOW,
+            EnemyTypes.PALMERS,
+            EnemyTypes.PALMERS,
+            EnemyTypes.PALMERS,
+            EnemyTypes.PALMERS,
         },
         fruitName = "Cucumber",
         fruitFile = "assets/graphcs/cucumber.png",
         fruitScore = 1000,
-        tick = 0.1,
-        enemySpeed = 120
+        tick = 0.05,
+        enemySpeed = 110
     },
     {
         enemies = {
-            EnemyTypes.SCARAB,
-            EnemyTypes.SCARAB,
-            EnemyTypes.SCARAB,
-            EnemyTypes.SCARAB,
+            EnemyTypes.WORM,
+            EnemyTypes.WORM,
+            EnemyTypes.WORM,
+            EnemyTypes.WORM,
         },
         fruitName = "Cucumber",
         fruitFile = "assets/graphcs/cucumber.png",
         fruitScore = 1000,
-        tick = 0.1,
+        tick = 0.04,
         enemySpeed = 120
     },
 }
